276f81902757b5c295ae6a6ba60ce1e9378caa46
Marcelle <Marcelle@Marcelle-PC>
2017-04-30 13:37:52 -0300

    atualização

---
+++ HomeCare/src/br/com/homecare/bean/FuncionarioBean.java
@@ -0,0 +1,22 @@
+package br.com.homecare.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+
+
+
+@ViewScoped
+@ManagedBean(name="funcionarioBean")
+public class FuncionarioBean extends AbstractBean implements Serializable{
+
+	private static final long serialVersionUID = 6712522758268465657L;
+	
+	@ManagedProperty("#{usuarioSessao}")
+	private UsuarioSessao usuarioSessao;
+
+	
+}
