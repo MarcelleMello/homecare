8aabe2307789b456d3281921801521bf75c58882
Thiago dos Santos Grillo <thiago10gr@gmail.com>
2017-05-26 14:34:44 -0300

    ok

--- HomeCare/src/br/com/homecare/bean/ConsultarFuncionarioBean.java
+++ HomeCare/src/br/com/homecare/bean/ConsultarFuncionarioBean.java
@@ -1,7 +1,6 @@
 package br.com.homecare.bean;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
